402c75296ff7b242d9aa61c5120a33c9de940f2a
gewton <gewtonj@br.ibm.com>
2016-11-21 12:46:53 -0300

    fixed some conditions like: when user refreshes page, when user is inactive for too long, etc. also added idle status (currently very low just for test)

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*eslint-env node */
 
+// TODO if user already in chat room, and enters again from another tab or whatever, do not show notification
+// TODO put all socket.io logic on routes.js, also logout logic
 //------------------------------------------------------------------------------
 // gewtonj-chat (Chat system by gewtonj@br.ibm.com)
 //------------------------------------------------------------------------------
@@ -19,6 +21,7 @@ var cfenv = require('cfenv');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+io.engine.pingTimeout = 60000 * 10; // 10 minutes without activity, user is disconnected
 
 //activating parser for http forms, using it as JSON in req.body 
 app.use(bodyParser.urlencoded({
@@ -51,36 +54,99 @@ io.use(function(socket, next) {
   sessionm(socket.request, socket.request.res, next);
 });
 app.use(sessionm);
+var status = {AVAILABLE:'av',BUSY:'b',AWAY:'aw'};
+var timeoutDisconnects = {};
+function disconnectUser(sock) {
+  sock.broadcast.emit('userdisconnect', {userid:sock.user.userid});
+  sock.disconnect();
+  delete timeoutDisconnects[sock.user.userid];
+}
+
 io.on('connection', function(socket){
   // Get user from session. If not found, disconnects.
-  var user = socket.request.session.user;
-  if(!user) {
+  if(!socket.request.session || !socket.request.session.user) {
     return socket.disconnect();
   }
+  
   // Saves user info in the socket room, so it can be retrieved later
-  socket.userid=user.userid;
-  // Sends a message to the recently-connected users with some initial data.
-  // Currently only data sent is the list of online users
+  socket.user={userid:socket.request.session.user.userid,status:status.AVAILABLE};
+  
+  // Check if user only recently lost connection. If positive, then no need to
+  // notify everyone that the user came back. Might have been just a page refresh.
+  if(timeoutDisconnects[socket.user.userid]) {
+    // User reconnected
+    clearTimeout(timeoutDisconnects[socket.user.userid]);
+    delete timeoutDisconnects[socket.user.userid];
+  } else {
+    // User connected
+    // Sends a message to everyone else that this user has connected, so they
+    // can update their "online" list
+    var userinfo = {};
+    userinfo[socket.user.userid] = socket.user.status;
+    socket.broadcast.emit('userconnect', userinfo);
+  }
+  
+  // Sends a message to the just connected (or just re-connected) user with some 
+  // initial data. Currently the only data sent is the list of online users
   socket.emit('initialdata', {onlineUsers:getOnlineUsers()});
-  // Now sends a message to everyone else that this user has connected, so they
-  // can update their "online" list
-  socket.broadcast.emit('userconnect', {userid:user.userid});
-  // Send everyone a message when it arrives
-  socket.on('newmessage', function(msg){
-    socket.broadcast.emit('newmessage', {userid:user.userid,msg:msg});
-  });
-  // Send everyone a signal that this user has disconnected
+
+  // If disconnected, starts a timeout of 1 minute. If no response after 1/2 minute,
+  // notify everyone that user has disconnected.
   socket.on('disconnect', function(){
-    socket.broadcast.emit('userdisconnect', {userid:user.userid});
+    timeoutDisconnects[socket.user.userid] = setTimeout(function(){
+      disconnectUser(socket);
+    }, 30000);
+  });
+  
+  // Triggered when user has no activity after a while, broadcasts to everyone
+  socket.on('idle', function(){
+    socket.user.status = status.AWAY;
+    io.emit('userstatus', socket.user);
+  });
+  
+  // Triggered when user marks itself as busy, broadcasts to everyone
+  socket.on('busy', function(){
+    socket.user.status = status.BUSY;
+    io.emit('userstatus', socket.user);
+  });
+  
+  // Triggered when user marks itself as available, broadcasts to everyone
+  socket.on('available', function(){
+    socket.user.status = status.AVAILABLE;
+    io.emit('userstatus', socket.user);
+  });
+  
+  // Triggered when a new message arrives
+  socket.on('newmessage', function(msg){
+    socket.broadcast.emit('newmessage', {userid:socket.user.userid,msg:msg});
+  });
+  
+  // On every heartbeat, check if user still logged on. If not, disconnect socket.
+  socket.conn.on('heartbeat', function() {
+    if(!socket.request.session || !socket.request.session.user) {
+      socket.disconnect();
+    }
   });
 });
+
+function getUserSockets(userid) {
+  var sockets = io.sockets.sockets;
+  var userSockets = [];
+  for(var key in sockets) {
+    var socket = sockets[key];
+    if(socket.user && socket.user.userid==userid) {
+      userSockets.push(socket);
+    }
+  }
+  return userSockets;
+}
 
 // Iterates over all connected sockets and return their "userid" attribute, set above
 function getOnlineUsers() {
   var sockets = io.sockets.sockets;
-  var users = [];
+  var users = {};
   for(var key in sockets) {
-    users.push(sockets[key].userid);
+    users[sockets[key].user.userid] = sockets[key].user.status;
   }
   return users;
 }
@@ -103,7 +169,16 @@ app.get('/', routes.index);
 app.get('/register', routes.registration);
 app.get('/login', routes.login);
 app.post('/login', routes.dologin);
-app.get('/logout', routes.logout);
+app.get('/logout', function(req, res){
+  if(req.session.user) {
+    var sockets = getUserSockets(req.session.user.userid);
+    for(var i = 0; i < sockets.length; i++) {
+      sockets[i].disconnect();
+    }
+    req.session.destroy();
+  }
+  res.redirect('/');
+});
 app.get('/chat', routes.chat);
 
 //get the app environment from Cloud Foundry
